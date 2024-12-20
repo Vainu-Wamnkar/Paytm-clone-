e7517d1406095b9d67d96d65c16728f03adabd72
ritik wamnkar <wamnkarr@gmail.com>
2024-12-20 08:38:03 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        nila:"#00baf2",
+        darkNila:"#002970",
+        lightColor:"#f5f5f5",
+        lightNila:"#00afe3",
+        lightdarkNila:"#0f4a8a",
+        bgColor:"#f5f7fa",
+        lightBlack:"#000000",
+        ultralightNila:"#4ccff6"
+      }
+    },
+  },
+  plugins: [],
+}
+
